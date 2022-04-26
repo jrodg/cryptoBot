883602e8e279d44335deb21429f19d25580067b3
juniorrodriguez <jrod.201@hotmail.com>
2022-04-26 10:42:02 -0400

    first commit

---
+++ coinmarket.py
@@ -0,0 +1,34 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+}
+
+session = Session()
+session.headers.update(headers)
+
+contracts = []
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+
+
+  def mngcontracts():
+
+    for contract in data['data']:
+        contracts.append(contract['symbol'])
+    return contracts
+
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
+print(mngcontracts())
